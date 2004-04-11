f8e8798c6a2b05c7628ea2f2f9ee38c4e4c97bd6
acetyl <acetyl>
2004-04-11 23:08:07 +0000

    added some font locking

--- simple-wiki.el
+++ simple-wiki.el
@@ -34,24 +34,97 @@
 ;; (add-to-list 'auto-mode-alist '("w3mtmp" . simple-wiki-mode))
 
 ;;History
-;; 1.0.1 
+;; 1.0.1
 ;;  -- add a variable to set the WikiName Regex
 
 ;;; Code:
-(defvar simple-wiki-link-pattern  "\\<[A-Z\xc0-\xde]+[a-z\xdf-\xff]+\\([A-Z\xc0-\xde]+[a-z\xdf-\xff]*\\)+\\>"
- "The pattern used for finding WikiName.")
+
+(defconst simple-wiki-link-pattern
+  "\\<[A-Z\xc0-\xde]+[a-z\xdf-\xff]+\\([A-Z\xc0-\xde]+[a-z\xdf-\xff]*\\)+\\>"
+  "The pattern used for finding WikiName.")
+
+
+(defgroup simple-wiki ()
+  "Edit raw wiki pages.")
+
+(defgroup mldonkey-faces ()
+  "Faces simple-wiki-mode." :group 'simple-wiki)
+
+(defface simple-wiki-heading-1-face
+  '((((type tty pc) (class color)) (:foreground "yellow" :weight bold))
+    (t (:height 1.2 :inherit simple-wiki-heading-2-face)))
+  "Face for WiKi headings at level 1."
+  :group 'simple-wiki-faces)
+
+(defface simple-wiki-heading-2-face
+  '((((type tty pc) (class color)) (:foreground "lightblue" :weight bold))
+    (t (:height 1.2 :inherit simple-wiki-heading-3-face)))
+  "Face for WiKi headings at level 2."
+  :group 'simple-wiki-faces)
+
+(defface simple-wiki-heading-3-face
+  '((((type tty pc) (class color)) (:weight bold))
+    (t (:height 1.2 :inherit simple-wiki-heading-4-face)))
+  "Face for WiKi headings at level 3."
+  :group 'simple-wiki-faces)
+
+(defface simple-wiki-heading-4-face
+  '((((type tty pc) (class color)) (:weight bold))
+    (t (:weight bold :inherit variable-pitch)))
+  "Face for WiKi headings at level 4."
+  :group 'simple-wiki-faces)
+
+(defface simple-wiki-heading-5-face
+  '((((type tty pc) (class color)) (:weight bold))
+    (t (:weight bold :inherit variable-pitch)))
+  "Face for WiKi headings at level 5."
+  :group 'simple-wiki-faces)
+
+(defface simple-wiki-heading-6-face
+  '((((type tty pc) (class color)) (:weight bold))
+    (t (:weight bold :inherit variable-pitch)))
+  "Face for WiKi headings at level 6."
+  :group 'simple-wiki-faces)
+
+(defface simple-wiki-code-face
+  '((((class color) (background dark)) (:background "dark slate gray"))
+    (((class color) (background light)) (:background "moccasin")))
+  "Face for code in Wiki pages."
+  :group 'simple-wiki-faces)
+
+(defconst simple-wiki-font-lock-keywords
+  '(("^\\([0-9]+\\)[ \t]+\\(#.+?\\)\n"
+     (1 'font-lock-constant-face)
+     (2 'font-lock-warning-face))       ; the time stamp and warning at the top
+    ("[^=]=\\([^\n=]+\\)=[^=]"
+
+     ;; headings
+     (1 'simple-wiki-heading-1-face))
+    ("[^=]=\\{2\\}\\([^\n=]+\\)=\\{2\\}[^=]"
+     (1 'simple-wiki-heading-2-face))
+    ("[^=]=\\{3\\}\\([^\n=]+\\)=\\{3\\}[^=]"
+     (1 'simple-wiki-heading-3-face))
+    ("[^=]=\\{4\\}\\([^\n=]+\\)=\\{4\\}[^=]"
+     (1 'simple-wiki-heading-4-face))
+    ("[^=]=\\{5\\}\\([^\n=]+\\)=\\{5\\}[^=]"
+     (1 'simple-wiki-heading-5-face))
+    ("[^=]=\\{6\\}\\([^\n=]+\\)=\\{6\\}[^=]"
+     (1 'simple-wiki-heading-6-face))
+
+    ("^ .+?$" . 'simple-wiki-code-face)                    ; code
+    ("<\\(/?[a-z]+\\)" (1 font-lock-function-name-face))   ; tags
+    ("^[*#]\\([*#]+\\)" . 'font-lock-constant-face)        ; enums
+    ("^\\([*#]\\)[^*#]" 1 font-lock-builtin-face)))        ; enums
 
 
 (define-derived-mode simple-wiki-mode text-mode "Wiki"
   "Simple mode to edit wiki pages.
 
 \\{simple-wiki-mode-map}"
-  (font-lock-add-keywords
-   nil
-   '(("^ .+?$" . font-lock-comment-face)
-     ("<\\(/?[a-z]+\\)" 1 font-lock-function-name-face)
-     ("^[*#]\\([*#]+\\)" . font-lock-constant-face)
-     ("^\\([*#]\\)[^*#]" 1 font-lock-builtin-face)))
+  (make-local-variable 'font-lock-defaults)
+  (setq font-lock-defaults  '(simple-wiki-font-lock-keywords t))
+
+  ;; FIXME: how to get this into `simple-wiki-font-lock-keywords'?
   (font-lock-add-keywords
    nil
    (list (cons (symbol-value 'simple-wiki-link-pattern)
@@ -64,6 +137,7 @@
 	 ("pre" \n) ("tt") ("u")))
   (set (make-local-variable 'skeleton-transformation) 'identity)
   (setq indent-tabs-mode nil))
+
 
 (provide 'simple-wiki)
 
